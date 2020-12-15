b4463ed7ec030f2a2508297daa5aee3f12a69ca5
miniongo3o <dlalsdyd094@naver.com>
2020-12-16 01:43:26 +0900

    1216

---
+++ assets/demo/chart-bar-demo.js
@@ -0,0 +1,66 @@
+// Set new default font family and font color to mimic Bootstrap's default styling
+Chart.defaults.global.defaultFontFamily = '-apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif';
+Chart.defaults.global.defaultFontColor = '#292b2c';
+
+// Bar Chart Example
+
+var graph_var = new XMLHttpRequest();
+    var arr = [];
+    graph_var.open("GET", "https://uvrmnxwp43.execute-api.us-east-1.amazonaws.com/icu-get-cheat-count-for-graph");
+    graph_var.send();
+    graph_var.onreadystatechange = function () {
+
+      if (graph_var.readyState !== XMLHttpRequest.DONE) return;
+      if (graph_var.status === 200) {
+        robj = JSON.parse(graph_var.responseText);
+        
+        robj.forEach((robj) => {
+            arr.push(robj.count);      
+        });
+      } else {
+        console.log(`[${graph_var.status}] : ${graph_var.statusText}`);
+      }
+    };
+
+var ctx = document.getElementById("myBarChart");
+
+var myLineChart = new Chart(ctx, {
+  type: 'bar',
+  data: {
+    labels: ["시선", "이어폰", "책상위", "사람"],
+    datasets: [{
+      label: "Revenue",
+      backgroundColor: "rgba(2,117,216,1)",
+      borderColor: "rgba(2,117,216,1)",
+      data: [Number(arr[0]), Number(arr[1]), 1, 4],
+    }],
+  },
+  options: {
+    scales: {
+      xAxes: [{
+        time: {
+          unit: 'month'
+        },
+        gridLines: {
+          display: false
+        },
+        ticks: {
+          maxTicksLimit: 6
+        }
+      }],
+      yAxes: [{
+        ticks: {
+          min: 0,
+          max: 5,
+          maxTicksLimit: 5
+        },
+        gridLines: {
+          display: true
+        }
+      }],
+    },
+    legend: {
+      display: false
+    }
+  }
+});
