490232d86b9790105273a24e88d6331bc51836cc
miniongo3o <dlalsdyd094@naver.com>
2020-12-16 14:40:55 +0900

    1216-8

--- assets/demo/chart-pie-demo.js
+++ assets/demo/chart-pie-demo.js
@@ -15,49 +15,44 @@ var pie_var = new XMLHttpRequest();
         pie_obj = JSON.parse(pie_var.responseText);
         //console.log( pie_obj[1].cheat_num );
         
-        pie_obj.forEach((element) => {
-          
-          temp_arr.push(element.count);
-            
+        for(const key in pie_obj)
+        {
+          temp_arr.push(pie_obj[key]);
+        }
+        
+        var ctx = document.getElementById("myPieChart");
+        var myPieChart = new Chart(ctx, {
+          type: 'doughnut',
+          data: {
+            labels: ["시선dlehd", "디바이스", "사람감지","음성감지"],
+            datasets: [{
+              data: [pie_obj[0].count, pie_obj[1].count, pie_obj[2].count, pie_obj[3].count],
+              backgroundColor: ['#00FF99', '#0099FF', '#00FFCC','#66FFFF'],
+              hoverBackgroundColor: ['#2e59d9', '#17a673', '#2c9faf'],
+              hoverBorderColor: "rgba(234, 236, 244, 1)",
+            }],
+          },
+          options: {
+            maintainAspectRatio: false,
+            tooltips: {
+              backgroundColor: "rgb(255,255,255)",
+              bodyFontColor: "#858796",
+              borderColor: '#dddfeb',
+              borderWidth: 1,
+              xPadding: 15,
+              yPadding: 15,
+              displayColors: false,
+              caretPadding: 10,
+            },
+            legend: {
+              display: false
+            },
+            cutoutPercentage: 80,
+          },
         });
+        
+
       } else {
         console.log(`[${pie_var.status}] : ${pie_var.statusText}`);
       }
     };
-
-
-console.log(temp_arr);
-console.log(temp_arr['0']);
-console.log(temp_arr[0]);
-
-
-var ctx = document.getElementById("myPieChart");
-var myPieChart = new Chart(ctx, {
-  type: 'doughnut',
-  data: {
-    labels: ["시선이동", "디바이스착용", "사람감지"],
-    datasets: [{
-      data: [55, 30, 15],
-      backgroundColor: ['#4e73df', '#1cc88a', '#36b9cc'],
-      hoverBackgroundColor: ['#2e59d9', '#17a673', '#2c9faf'],
-      hoverBorderColor: "rgba(234, 236, 244, 1)",
-    }],
-  },
-  options: {
-    maintainAspectRatio: false,
-    tooltips: {
-      backgroundColor: "rgb(255,255,255)",
-      bodyFontColor: "#858796",
-      borderColor: '#dddfeb',
-      borderWidth: 1,
-      xPadding: 15,
-      yPadding: 15,
-      displayColors: false,
-      caretPadding: 10,
-    },
-    legend: {
-      display: false
-    },
-    cutoutPercentage: 80,
-  },
-});
